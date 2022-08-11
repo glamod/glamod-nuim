031db19a8f75c00a90d2c7e146a4ff4f01b3f5f8
Robert Dunn <robert.dunn@metoffice.gov.uk>
2022-08-11 15:45:14 +0100

    #56: refactored step 3 (tidying up)

--- PYTHON_CDM_Conversion_code/Monthly_update_code/gsom_update_step3_jasmin.py
+++ PYTHON_CDM_Conversion_code/Monthly_update_code/gsom_update_step3_jasmin.py
@@ -5,18 +5,25 @@ Created on Thu Aug 27 09:40:50 2020
 
 @author: snoone
 """
-# remove all unwanted files after processing
+# step 3 remove all unwanted files after processing
 import os
 import glob
 
-files = glob.glob('/gws/nopw/j04/c3s311a_lot2/data/incoming/monthly_gsom_update/out/*.csv')
+# add parent directory to access daily conversion scripts
+sys.path.append("../")
+import utils
+
+from gsom_update_step1_jasmin import EXTENSION
+
+files = glob.glob(f"{utils.MONTHLY_UPDATE_EXTRACTDIR}*.{EXTENSION}")
 for f in files:
     os.remove(f)
 
-files = glob.glob('/gws/nopw/j04/c3s311a_lot2/data/incoming/monthly_gsom_update/out2/*.csv')
+files = glob.glob(f"{utils.MONTHLY_UPDATE_TEMPDIR}*.{EXTENSION}")
 for f in files:
     os.remove(f)
 
-files = glob.glob('/gws/nopw/j04/c3s311a_lot2/data/incoming/monthly_gsom_update/*.gz')
+# be explicit on the tar.gz file that's been downloaded
+files = glob.glob(f"{utils.MONTHLY_UPDATE_OUTDIR}{utils.MONTHLY_UPDATE_FILE}")
 for f in files:
     os.remove(f)
